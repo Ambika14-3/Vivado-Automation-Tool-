eedbcc1a0ace7470cfd8274a65911c41e1f31e29
Ambika Lakhera <ambika.vikas.lakhera@gmail.com>
2023-02-25 23:39:16 +0530

    Update tcl_run.tcl

--- Script/tcl_run.tcl
+++ Script/tcl_run.tcl
@@ -2,15 +2,26 @@ open_project [pwd]/Automation_demo/Automation_demo.xpr
 set_property source_mgmt_mode None [current_project]
 update_compile_order -fileset sources_1	
 
-set_property top [string trimright [lindex $argv 0] ".v"] [current_fileset]
-set_property source_mgmt_mode None [current_project]
+set fname [lindex $argv 0]
+set suffix [file extension $fname]
+set fname_wo_extension [string trimright $fname $suffix]
+
+set_property top $fname_wo_extension [current_fileset]
 update_compile_order -fileset sources_1
 
+
+
+puts $fname
+puts $suffix
+puts $fname_wo_extension
+
+
 reset_run synth_1
-launch_runs synth_1 -jobs 12
+launch_runs synth_1 -jobs 16
 
 reset_run impl_1
-launch_runs impl_1 -jobs 12
+launch_runs impl_1 -jobs 16
+
 
 set x 0
 while {$x !=1 } {
@@ -20,8 +31,9 @@ while {$x !=1 } {
 	else { set x 1 }
 }
 
-report_power > Impl_Automation_demo/[string trimright [lindex $argv 0] ".v"]/power.txt
-report_timing > Impl_Automation_demo/[string trimright [lindex $argv 0] ".v"]/timing.txt
-report_utilization > Impl_Automation_demo/[string trimright [lindex $argv 0] ".v"]/utilization.txt
+
+report_power > Impl_Automation_demo/$fname_wo_extension/power.txt
+report_timing > Impl_Automation_demo/$fname_wo_extension/timing.txt
+report_utilization > Impl_Automation_demo/$fname_wo_extension/utilization.txt
 
 close_project
